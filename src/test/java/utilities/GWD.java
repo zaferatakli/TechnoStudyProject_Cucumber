14f29f29cde8eba983a7938e60dfcaa5c2f96036
zaferatakli <zatakli@gmail.com>
2025-04-22 10:52:03 +1000

    GWD updated.

--- src/test/java/utilities/GWD.java
+++ src/test/java/utilities/GWD.java
@@ -1,23 +1,40 @@
 package utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.time.Duration;
 import java.util.Locale;
 
 public class GWD {
     private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
-    private static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
+    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
-        /// her cagirildiginda driver null ise yeni bir tane olusturacak
         if (threadDriver.get() == null) {
-            threadDriver = new ThreadLocal<>();
+            String browser = threadBrowserName.get();
+
+            if (browser == null) {
+                browser = "chrome"; // default tarayıcı
+            }
+
+            switch (browser.toLowerCase()) {
+                case "edge":
+                    threadDriver.set(new EdgeDriver());
+                    break;
+                case "firefox":
+                    threadDriver.set(new FirefoxDriver());
+                    break;
+                default:
+                    threadDriver.set(new ChromeDriver());
+            }
             threadDriver.get().manage().window().maximize();
-            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-            threadDriver.set(threadDriver.get());
+            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         }
         return threadDriver.get();
     }
@@ -28,10 +45,10 @@ public class GWD {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
         if (threadDriver.get() != null) {
             threadDriver.get().quit();
             threadDriver.remove();
         }
     }
 }
+
