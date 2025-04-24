466546cdee27301e73a61130a103d0eacf31ab0d
zaferatakli <zatakli@gmail.com>
2025-04-24 16:53:18 +1000

    DropDownMenu.feature and CrossCampusSteps added to project and tested.

--- src/test/java/stepDefinitions/BookingSteps.java
+++ src/test/java/stepDefinitions/BookingSteps.java
@@ -1,9 +1,6 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -23,11 +20,11 @@ public class BookingSteps {
 
     @And("the user accepts cookies")
     public void theUserAcceptsCookies() {
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("acceptCookiesButton")));
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.acceptCookiesButton));
 
         try {
-            if (dc.getWebElement("acceptCookiesButton").isDisplayed()) {
-                dc.myClick(dc.getWebElement("acceptCookiesButton"));
+            if (dc.acceptCookiesButton.isDisplayed()) {
+                dc.myClick(dc.acceptCookiesButton);
             }
         } catch (Exception e) {
             System.out.println("Accept Cookies button not displayed");
@@ -36,14 +33,13 @@ public class BookingSteps {
 
     @And("the Apply Now button is clearly visible")
     public void theButtonIsClearlyVisible() {
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("applyNowButton")));
-        Assert.assertTrue(dc.getWebElement("applyNowButton").isDisplayed());
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.applyNowButton));
+        Assert.assertTrue(dc.applyNowButton.isDisplayed());
     }
 
     @When("the user clicks the Apply Now button")
     public void theUserClicksTheButton() {
-        dc.action.moveToElement(dc.getWebElement("applyNowButton")).click().build().perform();
-        //dc.myClick(dc.getWebElement("applyNowButton"));
+        dc.action.moveToElement(dc.applyNowButton).click().build().perform();
     }
 
     @Then("the application form page is displayed")
@@ -54,45 +50,44 @@ public class BookingSteps {
 
     @When("the user fills in the form with the following details:")
     public void theUserFillsInTheFormWithTheFollowingDetails() {
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("nameInput")));
-        dc.mySendKeys(dc.getWebElement("nameInput"), (ConfigReader.getProperty("name")));
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.nameInput));
+        dc.mySendKeys(dc.nameInput, (ConfigReader.getProperty("name")));
 
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("emailInput")));
-        dc.mySendKeys(dc.getWebElement("emailInput"), (ConfigReader.getProperty("email")));
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.emailInput));
+        dc.mySendKeys(dc.emailInput, (ConfigReader.getProperty("email")));
 
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("phoneInput")));
-        dc.mySendKeys(dc.getWebElement("phoneInput"), (ConfigReader.getProperty("phoneNumber")));
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.phoneInput));
+        dc.mySendKeys(dc.phoneInput, (ConfigReader.getProperty("phoneNumber")));
 
-        Select selectCountry = new Select(dc.getWebElement("countrySelect"));
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("countrySelect")));
+        Select selectCountry = new Select(dc.countrySelect);
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.countrySelect));
         selectCountry.selectByVisibleText(ConfigReader.getProperty("country"));
 
-        Select selectCourse = new Select(dc.getWebElement("courseSelect"));
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("courseSelect")));
+        Select selectCourse = new Select(dc.courseSelect);
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.courseSelect));
         selectCourse.selectByVisibleText(ConfigReader.getProperty("course"));
 
-        Select selectHearAboutUsSelect = new Select(dc.getWebElement("hearAboutUsSelect"));
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("hearAboutUsSelect")));
+        Select selectHearAboutUsSelect = new Select(dc.hearAboutUsSelect);
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.hearAboutUsSelect));
         selectHearAboutUsSelect.selectByVisibleText(ConfigReader.getProperty("hearAboutUs"));
     }
 
     @And("the user click the terms of use checkbox")
     public void theUserClickTheTermsOfUseCheckbox() {
-        GWD.getWait().until(ExpectedConditions.elementToBeClickable(dc.getWebElement("checkBox")));
-        dc.myClick(dc.getWebElement("checkBox"));
+        GWD.getWait().until(ExpectedConditions.elementToBeClickable(dc.checkBox));
+        dc.myClick(dc.checkBox);
     }
 
     @And("the user clicks the Book a Call button")
     public void theUserClicksTheBookACallButton() {
-        GWD.getWait().until(ExpectedConditions.elementToBeClickable(dc.getWebElement("bookCallButton")));
-        dc.myClick(dc.getWebElement("bookCallButton"));
+        GWD.getWait().until(ExpectedConditions.elementToBeClickable(dc.bookCallButton));
+        dc.myClick(dc.bookCallButton);
     }
 
     @Then("a confirmation message is displayed")
     public void aConfirmationMessageIsDisplayed() {
         dc.action.pause(Duration.ofSeconds(30)).build().perform();
-        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.getWebElement("thankYouMessage")));
-        Assert.assertTrue(dc.getWebElement("thankYouMessage").isDisplayed());
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.thankYouMessage));
+        Assert.assertTrue(dc.thankYouMessage.isDisplayed());
     }
 }
-
--- src/test/java/stepDefinitions/DropDownMenuSteps.java
+++ src/test/java/stepDefinitions/DropDownMenuSteps.java
@@ -1,4 +1,52 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import pages.DialogContent;
+import pages.HeaderMenu;
+import utilities.GWD;
+
 public class DropDownMenuSteps {
+    HeaderMenu hm = new HeaderMenu();
+    DialogContent dc = new DialogContent();
+
+    @When("The user hovers over the Programs menu")
+    public void theUserHoversOverTheProgramsMenu() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.programs));
+        hm.action.moveToElement(hm.programs).build().perform();
+    }
+
+    @Then("The user should be see the all programs link and selects each page and navigated to the correct course page")
+    public void theUserShouldBeNavigatedToTheCorrectCoursePage() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.sdetLink));
+        hm.sdetLink.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.softwareHomepage));
+        Assert.assertTrue(dc.softwareHomepage.isDisplayed(), "No text found");
+
+        hm.action.moveToElement(hm.programs).build().perform();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.dataScienceLink));
+        hm.dataScienceLink.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.dataScienceHomepage));
+        Assert.assertTrue(dc.dataScienceHomepage.isDisplayed(), "No text found");
+
+        hm.action.moveToElement(hm.programs).build().perform();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.artificialIntelligenceLink));
+        hm.artificialIntelligenceLink.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.artificialHomepage));
+        Assert.assertTrue(dc.artificialHomepage.isDisplayed(), "No text found");
+
+        hm.action.moveToElement(hm.programs).build().perform();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.androidDeveloperLink));
+        hm.androidDeveloperLink.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.androidDevHomepage));
+        Assert.assertTrue(dc.androidDevHomepage.isDisplayed(), "No text found");
+
+        hm.action.moveToElement(hm.programs).build().perform();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(hm.mastersProgramLink));
+        hm.mastersProgramLink.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.masterHomepage));
+        Assert.assertTrue(dc.masterHomepage.isDisplayed(), "No text found");
+    }
 }
+
