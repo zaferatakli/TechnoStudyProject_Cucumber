422a7e5c7a5e0e37e6169f95209cbd7b7a235d2f
NuriOzturk <nroztrk17@gmail.com>
2025-04-26 00:25:29 +0300

    BookingSteps updated.

--- src/test/java/stepDefinitions/BookingSteps.java
+++ src/test/java/stepDefinitions/BookingSteps.java
@@ -1,6 +1,10 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -9,9 +13,12 @@ import utilities.ConfigReader;
 import utilities.GWD;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 public class BookingSteps {
     DialogContent dc = new DialogContent();
+    Random random = new Random();
 
     @Given("the user navigate the website")
     public void theUserIsOnTheHomepage() {
@@ -63,13 +70,19 @@ public class BookingSteps {
         GWD.getWait().until(ExpectedConditions.visibilityOf(dc.countrySelect));
         selectCountry.selectByVisibleText(ConfigReader.getProperty("country"));
 
-        Select selectCourse = new Select(dc.courseSelect);
         GWD.getWait().until(ExpectedConditions.visibilityOf(dc.courseSelect));
-        selectCourse.selectByVisibleText(ConfigReader.getProperty("course"));
+        Select selectCourse = new Select(dc.courseSelect);
+        List<WebElement> courseOptions = selectCourse.getOptions();
+        int randomCourse = random.nextInt(courseOptions.size() - 1) + 1;
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.courseSelect));
+        selectCourse.selectByIndex(randomCourse);
 
-        Select selectHearAboutUsSelect = new Select(dc.hearAboutUsSelect);
         GWD.getWait().until(ExpectedConditions.visibilityOf(dc.hearAboutUsSelect));
-        selectHearAboutUsSelect.selectByVisibleText(ConfigReader.getProperty("hearAboutUs"));
+        Select selectHearAboutUsSelect = new Select(dc.hearAboutUsSelect);
+        List<WebElement> hearAboutUsOptions = selectHearAboutUsSelect.getOptions();
+        int randomHearAboutUs = random.nextInt(hearAboutUsOptions.size() - 1) + 1;
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.hearAboutUsSelect));
+        selectHearAboutUsSelect.selectByIndex(randomHearAboutUs);
     }
 
     @And("the user click the terms of use checkbox")
@@ -91,3 +104,4 @@ public class BookingSteps {
         Assert.assertTrue(dc.thankYouMessage.isDisplayed());
     }
 }
+
